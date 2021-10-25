8705d8757e5bba3d71634600b16e0d7ba1487023
Meg <meg@elias.fritz.box>
2021-10-25 05:30:27 +0200

    requirements.txt

--- seasonality.py
+++ seasonality.py
@@ -1,106 +1,97 @@
-#!/usr/bin/env python
-# coding: utf-8
+# |------------------------------------------------------------------
+# | # Tunnel Traffic  -- Time Sequence Analysis T1
+# |------------------------------------------------------------------
+# |
+# | ## 1. Introduction
+# |
+# | This is a notebook to practice routine procedures
+# | in the time sequence analysis.
+# |
+# | Temporal sequence consists of several components.
+# |
+# | - trend (gradual decrease / increase)
+# | - seaonality (hour of day, day of week, week of month, month of year)
+# | - cycle (up and down but with a specific time scale)
+# | - pecuriality (national holidays)
+# |
+# | In a sense these are the only elements that one can predict
+# | by mahcine learning, and nothign else.
+# | A  model can predict  what only  repeats. What happened before.
+# | A model cannot learn from what did not happen.
+# |
+# | To deal with each elemetns of time sequence, we have
+# |
+# | - __trend__ : Analytical fitting of baseline (lienar, polynomial, etc)
+# | - __seasonality__ : Fourier decomposition
+# | - __cycle__ : Lags
+# | - __pecuriality__ : Categorical featuers
+# |
+# | In this notebook we will familarize ourselve with
+# |
+# |  * Manupulation of index.
+# |  * 'DeterministicProcess` in 'statsmodels' package as 'time dummy'.
+# |
+# | The only features (except categorical ones) used to model the temporal
+# | behaviour of the target is time. But time in different intervals.
+# | 'DeterministicProcess` is used to quickly create `t` in `y=f(t)`.
+# | It is called 'deterministic', because it is a feature that we do not
+# | have a control. One can use such features at the time of prediction, i.e.
+# | if we would like to predict a sales on a sunday, we can use the fact
+# | that day is a sunday. On the contrary, we cannot use a sales one day before
+# | if it is not published yet at the time the prediction is to be executed.
+# | The former (=being a Sunday) is a dterministic feature, while the sales on
+# | Saturday is a non-deterministic feature.
+# |
+# | ## 2. Task
+# |
+# |   We have a record of trafic in a tunnel. Model the temporal sequence of
+# | the trafic from the time features only.
 
-# # What is Seasonality? #
-# 
-# We say that a time series exhibits **seasonality** whenever there is a regular, periodic change in the mean of the series. Seasonal changes generally follow the clock and calendar -- repetitions over a day, a week, or a year are common. Seasonality is often driven by the cycles of the natural world over days and years or by conventions of social behavior surrounding dates and times.
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/ViYbSxS.png" width=800, alt="">
-# <figcaption style="textalign: center; font-style: italic"><center>Seasonal patterns in four time series.
-# </center></figcaption>
-# </figure>
-# 
-# We will learn two kinds of features that model seasonality. The first kind, indicators, is best for a season with few observations, like a weekly season of daily observations. The second kind, Fourier features, is best for a season with many observations, like an annual season of daily observations.
-# 
-# # Seasonal Plots and Seasonal Indicators #
-# 
-# Just like we used a moving average plot to discover the trend in a series, we can use a **seasonal plot** to discover seasonal patterns.
-# 
-# A seasonal plot shows segments of the time series plotted against some common period, the period being the "season" you want to observe. The figure shows a seasonal plot of the daily views of Wikipedia's article on *Trigonometry*: the article's daily views plotted over a common *weekly* period.
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/bd7D4NJ.png" width=800, alt="">
-# <figcaption style="textalign: center; font-style: italic"><center>There is a clear weekly seasonal pattern in this series, higher on weekdays and falling towards the weekend.
-# </center></figcaption>
-# </figure>
-# 
-# ### Seasonal indicators
-# 
-# **Seasonal indicators** are binary features that represent seasonal differences in the level of a time series. Seasonal indicators are what you get if you treat a seasonal period as a categorical feature and apply one-hot encoding.
-# 
-# By one-hot encoding days of the week, we get weekly seasonal indicators. Creating weekly indicators for the *Trigonometry* series will then give us six new "dummy" features. (Linear regression works best if you drop one of the indicators; we chose Monday in the frame below.)
-# 
-# | Date       | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday |
-# |------------|---------|-----------|----------|--------|----------|--------|
-# | 2016-01-04 | 0.0     | 0.0       | 0.0      | 0.0    | 0.0      | 0.0    |
-# | 2016-01-05 | 1.0     | 0.0       | 0.0      | 0.0    | 0.0      | 0.0    |
-# | 2016-01-06 | 0.0     | 1.0       | 0.0      | 0.0    | 0.0      | 0.0    |
-# | 2016-01-07 | 0.0     | 0.0       | 1.0      | 0.0    | 0.0      | 0.0    |
-# | 2016-01-08 | 0.0     | 0.0       | 0.0      | 1.0    | 0.0      | 0.0    |
-# | 2016-01-09 | 0.0     | 0.0       | 0.0      | 0.0    | 1.0      | 0.0    |
-# | 2016-01-10 | 0.0     | 0.0       | 0.0      | 0.0    | 0.0      | 1.0    |
-# | 2016-01-11 | 0.0     | 0.0       | 0.0      | 0.0    | 0.0      | 0.0    |
-# | ...        | ...     | ...       | ...      | ...    | ...      | ...    |
+# | ## 3. Data
+# |
+# | 1. A traffic of vehicles traveling through the Baregg Tunnel
+# | in Switzerland each day from November 2003 to November 2005.
+#
+# | ## 3. Notebook
+# | -------------------------------------
+# | Import packages.
 
-# Adding seasonal indicators to the training data helps models distinguish means within a seasonal period:
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/hIlF5j5.png" width=800, alt="">
-# <figcaption style="textalign: center; font-style: italic"><center>Ordinary linear regression learns the mean values at each time in the season.</center></figcaption>
-# </figure>
-# 
-# The indicators act as On / Off switches. At any time, at most one of these indicators can have a value of `1` (*On*). Linear regression learns a baseline value `2379` for `Mon` and then adjusts by the value of whichever indicator is *On* for that day; the rest are `0` and vanish.
-# 
-# # Fourier Features and the Periodogram #
-# 
-# The kind of feature we discuss now are better suited for long seasons over many observations where indicators would be impractical. Instead of creating a feature for each date, Fourier features try to capture the overall shape of the seasonal curve with just a few features.
-# 
-# Let's take a look at a plot for the annual season in *Trigonometry*. Notice the repetitions of various frequencies: a long up-and-down movement three times a year, short weekly movements 52 times a year, and perhaps others.
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/NJcaEdI.png" width=800, alt="">
-# <figcaption style="textalign: center; font-style: italic"><center>Annual seasonality in the <em>Wiki Trigonometry</em> series.</center></figcaption>
-# </figure>
-# 
-# It is these frequencies within a season that we attempt to capture with Fourier features. The idea is to include in our training data periodic curves having the same frequencies as the season we are trying to model. The curves we use are those of the trigonometric functions sine and cosine.
-# 
-# **Fourier features** are pairs of sine and cosine curves, one pair for each potential frequency in the season starting with the longest. Fourier pairs modeling annual seasonality would have frequencies: once per year, twice per year, three times per year, and so on.
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/bKOjdU7.png" width=600, alt="A top figure and a bottom figure, each showing a sine curve and a cosine curve. The curves in the top plot both have frequency of once per year, while the curves in the bottom plot both have a frequency of twice per year.">
-# <figcaption style="textalign: center; font-style: italic"><center>The first two Fourier pairs for annual seasonality. <strong>Top: </strong>Frequency of once per year. <strong>Bottom: </strong>Frequency of twice per year.</strong></center></figcaption>
-# </figure>
-# 
-# If we add a set of these sine / cosine curves to our training data, the linear regression algorithm will figure out the weights that will fit the seasonal component in the target series. The figure illustrates how linear regression used four Fourier pairs to model the annual seasonality in the *Wiki Trigonometry* series.
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/mijPhko.png" width=600, alt="">
-# <figcaption style="textalign: center; font-style: italic"><center><strong>Top: </strong>Curves for four Fourier pairs, a sum of sine and cosine with regression coefficients. Each curve models a different frequency. <strong>Bottom: </strong>The sum of these curves approximates the seasonal pattern.</center></figcaption>
-# </figure>
-# 
-# Notice that we only needed eight features (four sine / cosine pairs) to get a good estimate of the annual seasonality. Compare this to the seasonal indicator method which would have required hundreds of features (one for each day of the year). By modeling only the "main effect" of the seasonality with Fourier features, you'll usually need to add far fewer features to your training data, which means reduced computation time and less risk of overfitting.
-# 
-# ### Choosing Fourier features with the Periodogram
-# 
-# How many Fourier pairs should we actually include in our feature set? We can answer this question with the periodogram. The **periodogram** tells you the strength of the frequencies in a time series. Specifically, the value on the y-axis of the graph is `(a ** 2 + b ** 2) / 2`, where `a` and `b` are the coefficients of the sine and cosine at that frequency (as in the *Fourier Components* plot above).
-# 
-# <figure style="padding: 1em;">
-# <img src="https://i.imgur.com/PK6WEe3.png" width=600, alt="">
-# <figcaption style="textalign: center; font-style: italic"><center>Periodogram for the <em>Wiki Trigonometry</em> series.</center></figcaption>
-# </figure>
-# 
-# From left to right, the periodogram drops off after *Quarterly*, four times a year. That was why we chose four Fourier pairs to model the annual season. The *Weekly* frequency we ignore since it's better modeled with indicators.
-# 
-# ### Computing Fourier features (optional)
-# 
+import os
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
+
+import plotly.graph_objs as go
+from IPython.display import display
+
+import os
+
+
+import sklearn
+sklearn.__version__
+
+# | Set up directories.
+
+DATA_DIR = Path('../input/ts-courses-data')
+CWD
+
+# | Read the data, first as it is.
+
+tunnel = pd.read_csv(DATA_DIR/'tunnel.csv')
+
+# | Check the contents.
+
+print(tunnel.info())
+
+display(tunnel.head(3))
+
+
 # Knowing how Fourier features are computed isn't essential to using them, but if seeing the details would clarify things, the cell hidden cell below illustrates how a set of Fourier features could be derived from the index of a time series. (We'll use a library function from `statsmodels` for our applications, however.)
 
 # In[1]:
 
-
-
-import numpy as np
 
 def fourier_features(index, freq, order):
     time = np.arange(len(index), dtype=np.float32)
@@ -108,17 +99,13 @@ def fourier_features(index, freq, order):
     features = {}
     for i in range(1, order+1):
         features.update({
-            f'sin_{freq}_{i}': np.sin(i*k), 
+            f'sin_{freq}_{i}': np.sin(i*k),
             f'cos_{freq}_{i}': np.cos(i*k),
         })
     return pd.DataFrame(features, index=index)
 
 
 # In[2]:
-
-
-
-import numpy as np
 
 
 def fourier_features(index, freq, order):
@@ -139,30 +126,15 @@ def fourier_features(index, freq, order):
 # fourier_features(y, freq=365.25, order=4)
 
 
-# # Example - Tunnel Traffic #
-# 
+# Example - Tunnel Traffic #
+#
 # We'll continue once more with the *Tunnel Traffic* dataset. This hidden cell loads the data and defines two functions: `seasonal_plot` and `plot_periodogram`.
 
 # In[ ]:
 
 
-
-
-
-
-
 # In[3]:
 
-
-
-from pathlib import Path
-from warnings import simplefilter
-
-import matplotlib.pyplot as plt
-import pandas as pd
-import seaborn as sns
-from sklearn.linear_model import LinearRegression
-from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
 
 simplefilter("ignore")
 
@@ -273,7 +245,7 @@ X["dayofyear"] = X.index.dayofyear
 X["year"] = X.index.year
 fig, (ax0, ax1) = plt.subplots(2, 1, figsize=(11, 6))
 seasonal_plot(X, y="NumVehicles", period="week", freq="day", ax=ax0)
-seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1);
+seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1)
 
 
 # Now let's look at the periodogram:
@@ -281,19 +253,18 @@ seasonal_plot(X, y="NumVehicles", period="year", freq="dayofyear", ax=ax1);
 # In[5]:
 
 
-plot_periodogram(tunnel.NumVehicles);
+plot_periodogram(tunnel.NumVehicles)
 
 
 # The periodogram agrees with the seasonal plots above: a strong weekly season and a weaker annual season. The weekly season we'll model with indicators and the annual season with Fourier features. From right to left, the periodogram falls off between *Bimonthly (6)* and *Monthly (12)*, so let's use 10 Fourier pairs.
-# 
+#
 # We'll create our seasonal features using `DeterministicProcess`, the same utility we used in Lesson 2 to create trend features. To use two seasonal periods (weekly and annual), we'll need to instantiate one of them as an "additional term":
 
 # In[6]:
 
 
-from statsmodels.tsa.deterministic import CalendarFourier, DeterministicProcess
-
-fourier = CalendarFourier(freq="A", order=10)  # 10 sin/cos pairs for "A"nnual seasonality
+# 10 sin/cos pairs for "A"nnual seasonality
+fourier = CalendarFourier(freq="A", order=10)
 
 dp = DeterministicProcess(
     index=tunnel.index,
@@ -312,7 +283,6 @@ X = dp.in_sample()  # create features for dates in tunnel.index
 # In[7]:
 
 
-
 y = tunnel["NumVehicles"]
 
 model = LinearRegression(fit_intercept=False)
@@ -322,18 +292,17 @@ y_pred = pd.Series(model.predict(X), index=y.index)
 X_fore = dp.out_of_sample(steps=90)
 y_fore = pd.Series(model.predict(X_fore), index=X_fore.index)
 
-ax = y.plot(color='0.25', style='.', title="Tunnel Traffic - Seasonal Forecast")
+ax = y.plot(color='0.25', style='.',
+            title="Tunnel Traffic - Seasonal Forecast")
 ax = y_pred.plot(ax=ax, label="Seasonal")
 ax = y_fore.plot(ax=ax, label="Seasonal Forecast", color='C3')
 _ = ax.legend()
 
 
-
-
 # ---
-# 
+#
 # There's still more we can do with time series to improve our forecasts. In the next lesson, we'll learn how to use time series themselves as a features. Using time series as inputs to a forecast lets us model the another component often found in series: *cycles*.
-# 
+#
 # # Your Turn #
-# 
+#
 # [**Create seasonal features for Store Sales**](https://www.kaggle.com/kernels/fork/19615991) and extend these techniques to capturing holiday effects.
